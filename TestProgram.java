8dc2232939a52f1e9cc93976834c39bc13495412
Suki Tsang <sukitcl@yahoo.com.hk>
2017-03-27 18:04:40 +0800

    Update TestProgram with more output variables

--- TestProgram.java
+++ TestProgram.java
@@ -1,10 +1,9 @@
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.PrintStream;
 
 import java.util.*;
 
@@ -20,9 +19,11 @@ public class TestProgram {
     private Indexer indexer;
     private DataManager pageID;  
     private DataManager pageInfo;
+    private int pageCount = 0;
 
  
     public TestProgram(String database) throws IOException {
+        // Open the database
 		recman = RecordManagerFactory.createRecordManager(database);
 		indexer = new Indexer();
 		pageID = new DataManager(recman, "pageID");		// URL to pageID mapping
@@ -35,17 +36,22 @@ public class TestProgram {
     }
 
     public void print() throws IOException {
-        PrintWriter pw = new PrintWriter("spider_result.txt");
-	    FastIterator it = pageID.getIterator();
+        // Put the result into a txt file
+        PrintStream pw = new PrintStream(new FileOutputStream("spider_result.txt"));
+        System.setOut(pw);
+	    /*FastIterator it = pageID.getIterator();
         String url = null;
 
         while((url = (String) it.next()) != null) 
 		{
-			//get the page_id given an url.
-			String pageid = (String) pageID.getEntry(url);
-			//pw.println(url);
-            pw.println(pageID.printAll());
-		}
+			//String pageid = (String) pageID.getEntry(url);
+           
+            pageCount++;
+            System.out.println(pageCount +":URL " +url);
+            System.out.println("-----------------------------------------");
+   
+		}*/
+        pageInfo.printAll();
 		pw.close();
     }
 
